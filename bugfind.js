a3673c0a05a9c295624a381a3aaf62eb2459307c
rubelstack <ariyankhanoyon396@gmail.com>
2026-07-30 00:34:42 +0600

    fixing bug 1

---
+++ bugfind.js
@@ -0,0 +1,18 @@
+function averageResponseTime(times) {
+  if (Array.isArray(times) === false) {
+        return "Invalid";
+   }
+   
+   if (times.length = 0) {
+        return "Invalid";
+    }
+   
+ let total = 0;
+    for (let i = 1; i <= times.length; i++) {
+        total = total + time[i];
+    }
+   
+  return total / times;
+}
+
+console.log(averageResponseTime([120, 200, 150, 130]));
