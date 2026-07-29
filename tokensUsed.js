b59c8267efd83d34b414cb3d219e29a2ef660974
rubelstack <ariyankhanoyon396@gmail.com>
2026-07-29 22:56:53 +0600

    problem 3 solved

---
+++ tokensUsed.js
@@ -0,0 +1,18 @@
+function calculateAiCost(tokensUsed) {
+
+if (typeof tokensUsed ==='number' ){
+    if(tokensUsed >=0){
+    let token_consumed= tokensUsed - 500;
+    let cost = Math.floor(token_consumed/100)*5
+    if (cost < 0){
+        return 0
+    } else return cost
+    } return "Invalid"
+       
+} else return "Invalid"
+
+
+}
+
+
+console.log(calculateAiCost(1000));
