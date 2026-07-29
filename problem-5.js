7922b9c96ca8b922f90383d4dfab84807ee94125
rubelstack <ariyankhanoyon396@gmail.com>
2026-07-30 00:41:32 +0600

    fixing bug 2

--- problem-5.js
+++ problem-5.js
@@ -1,17 +1,17 @@
 function averageResponseTime(times) {
   if (Array.isArray(times) === false) {
-        return "Invalid";
-   }
-   
-   if (times.length = 0) {
-        return "Invalid";
-    }
-   
- let total = 0;
-    for (let i = 1; i <= times.length; i++) {
-        total = total + time[i];
-    }
-   
+    return "Invalid";
+  }
+
+  if ((times.length === 0)) {
+    return "Invalid";
+  }
+
+  let total = 0;
+  for (let i = 1; i <= times.length; i++) {
+    total = total + time[i];
+  }
+
   return total / times;
 }
 
