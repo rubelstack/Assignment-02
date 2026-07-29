2bff75a508dfde8f2cd4555dbd0eb043b1e2af7f
rubelstack <ariyankhanoyon396@gmail.com>
2026-07-30 00:04:04 +0600

    prettier using

--- toprestaurant.js
+++ toprestaurant.js
@@ -1,29 +1,21 @@
 function topRatedRestaurant(restaurants) {
-if(!Array.isArray(restaurants) || restaurants.length === 0){
-    return "Invalid"
-}
-let toprestaurnt = ''
-let calculate = 0;
-for(const number of restaurants){
-    restaurantname= ''
-   let  name = number.name
-   let ratings = number.rating
-   if(ratings > calculate){
-    calculate += ratings
-    toprestaurnt = name
-   }
-
-}
-return toprestaurnt.toUpperCase()
-
-
-
-
-
-
+  if (!Array.isArray(restaurants) || restaurants.length === 0) {
+    return "Invalid";
+  }
+  let toprestaurnt = "";
+  let calculate = 0;
+  for (const number of restaurants) {
+    restaurantname = "";
+    let name = number.name;
+    let ratings = number.rating;
+    if (ratings > calculate) {
+      calculate += ratings;
+      toprestaurnt = name;
+    }
+  }
+  return toprestaurnt.toUpperCase();
 }
 
-
-const arr = []
+const arr = [];
 
 console.log(topRatedRestaurant(arr));
