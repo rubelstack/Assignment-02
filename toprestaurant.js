7d011bd9251c80ea76965955f6de60bc84001530
rubelstack <ariyankhanoyon396@gmail.com>
2026-07-30 00:03:20 +0600

    problem  4 solved

---
+++ toprestaurant.js
@@ -0,0 +1,29 @@
+function topRatedRestaurant(restaurants) {
+if(!Array.isArray(restaurants) || restaurants.length === 0){
+    return "Invalid"
+}
+let toprestaurnt = ''
+let calculate = 0;
+for(const number of restaurants){
+    restaurantname= ''
+   let  name = number.name
+   let ratings = number.rating
+   if(ratings > calculate){
+    calculate += ratings
+    toprestaurnt = name
+   }
+
+}
+return toprestaurnt.toUpperCase()
+
+
+
+
+
+
+}
+
+
+const arr = []
+
+console.log(topRatedRestaurant(arr));
